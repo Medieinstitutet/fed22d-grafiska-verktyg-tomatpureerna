57325fc29cc29838a921b6e077a36fc118556dd8
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-01-10 15:12:01 +0100

    :bricks: still building project

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
