777168f1d9963490530b8d3f96fb5f6d41fe97c7
MariaLBovin <maria.bovin@gmail.com>
2023-01-11 18:14:47 +0100

    /feat h2 and p added

--- src/slideshow.js
+++ src/slideshow.js
@@ -5,24 +5,29 @@ const productCardArray = [
     product: 'Vetemjölk',
     description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
     image: [
-        {
-            src:,
-            alt:,
-        },
-        {
-            src:,
-            alt:,
-        }
-    ]
-},
+      {
+        src: './images/wheatly_productcard.jpg',
+        alt: 'text',
+      }],
+  },
   {
     product: 'Vetegröt',
     description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
+    image: [
+      {
+        src: './images/wheatporridge_productcard.jpg',
+        alt: 'text',
+      }],
   },
   {
     product: 'Vetegryn',
     description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
-  }
+    image: [
+      {
+        src: './images/wheat_productcard_desktop.jpg',
+        alt: 'text',
+      }],
+  },
 ];
 
 const swiper = new Swiper('.mySwiper', {
@@ -34,3 +39,14 @@ const swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+function displayProducts() {
+  const productPlacement = document.querySelectorAll('.swiper-slide');
+
+  for (let i = 0; i < productCardArray.length; i++) {
+    const printProductcards = `<img class="product__image" id="img-${i}" src="${productCardArray[i].image}" alt="${productCardArray[i].image.alt}" width="260" height="185">
+                                <h2>${productCardArray[i].product}</h2>
+                                <p>${productCardArray[i].description}</p>`;
+    productPlacement[i].innerHTML = printProductcards;
+  }
+}
+displayProducts();
