4bc88fa8280b8931597faa28b0705312befb6912
Therese <therese.holen22@gmail.com>
2023-01-13 12:46:34 +0100

    /fix Changed the location of the JS files import due to slideshow.js blocking with a console error. /feat Added animation to mobile screen.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
 import './style.scss';
-import './slideshow.js';
 import './logoanimation.js';
-//import {gsap} from 'gsap';
-
-//console.log('hej');
-//const logoanimation = document.querySelector('#header__logo__id');
-//gsap.to('#header__logo__id', { x: 500, delay: 1, rotate: 720 });
+import './slideshow.js';
