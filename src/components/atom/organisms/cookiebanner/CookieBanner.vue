f37a89adee2d9d627c00f624486f08b9dac53c59
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-01-16 14:57:22 +0100

    /feat 💄Style Cookiebanner for tablet and desktop

--- src/components/atom/organisms/cookiebanner/CookieBanner.vue
+++ src/components/atom/organisms/cookiebanner/CookieBanner.vue
@@ -13,7 +13,7 @@ function hideCookies() {
 <template>
   <div class="cookiebanner" v-if="showCookieBanner">
     <p>Vi använder kakor för att kunna ge dig en bra upplevelse
-      när du besöker vår webbplats.Du kan välja att godkänna att
+      när du besöker vår webbplats. Du kan välja att godkänna att
       vi använder kakor under ditt besök genom att klicka på “Acceptera”</p>
     <ButtonAtom btn-msg="Acceptera" class="cookiebanner__button__accept" @btn-click="hideCookies"/>
   </div>
--- src/vue-main.js
+++ src/vue-main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import './style/style.scss';
 import App from './App.vue';
 
 createApp(App).mount('#app');
