5e4ccc7426d3b3fb387fd88541c9960a7d91a871
MariaLBovin <maria.bovin@gmail.com>
2023-01-16 11:30:50 +0100

    /feat slider mobile added

---
+++ src/AppSlideShow.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <div ref="swiperElement" class="swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="item in products"
+        :key="item.product">
+          <img :src="item.image[0].url" />
+          <div class="text-wrapper">
+          <h2>{{ item.product}}</h2>
+          <p>{{ item.description}}</p>
+        </div>
+        </div>
+      </div>
+      <button class="swiper-button-prev" @click="previousSlide"></button>
+    <button class="swiper-button-next" @click="nextSlide"></button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { onMounted, ref } from 'vue';
+import { Swiper } from 'swiper';
+import 'swiper/css/navigation';
+import vetemjolk from '/images/wheatly_desktop.jpg';
+import './style.scss';
+
+// Vi sparar HTML-elementet som innehåller våra slides i en variabel
+const swiperElement = ref(null);
+// Och så behöver vi en variabel för själva slideshowen
+const slideshow = ref(null);
+// När HTML-koden är redo så kallar vi på "setupSwiper"
+onMounted(() => {
+  setupSwiper();
+});
+
+function setupSwiper() {
+  // Spara swipern i en variabel som heter slideshow (vi måste skriva slideshow.value pga. Vue)
+  slideshow.value = new Swiper(swiperElement.value, {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 1,
+    breakpoints: {
+      744: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+  });
+}
+// Denna bläddrar till vänster och anropas från HTML-delen
+function previousSlide() {
+  slideshow.value.slidePrev();
+}
+// Denna bläddrar till höger och anropas från HTML-delen
+function nextSlide() {
+  slideshow.value.slideNext();
+}
+const products = [
+  {
+    product: 'Vetemjölk',
+    description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
+    image: [
+      {
+        url: vetemjolk,
+        alt: 'text',
+      }],
+  },
+  {
+    product: 'Vetegröt',
+    description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
+    image: [
+      {
+        url: vetemjolk,
+        alt: 'text',
+      }],
+  },
+  {
+    product: 'Vetegryn',
+    description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Mer lorem Ipsum för folket!',
+    image: [
+      {
+        url: vetemjolk,
+        alt: 'text',
+      }],
+  },
+];
+
+</script>
+<style>
+.swiper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 400px;
+  padding-left: 50px;
+  padding-right: 50px;
+  outline: solid blue 1px;
+}
+.swiper-wrapper {
+  outline: solid purple 1px;
+}
+.swiper-slide {
+  width: 70%;
+  height: auto;
+  margin-top:5px;
+  margin-bottom: 5px;
+  text-align: center;
+  font-size: 1rem;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 20px;
+  filter: drop-shadow(2px 8px 4px #18978F);
+  outline: solid red 1px;
+}
+.text-wrapper {
+  padding:20px;
+}
+h2 {
+  font-family: Rubik Mono One;
+  letter-spacing: 10%;
+}
+p {
+  font-family: Rubik;
+  letter-spacing: 10%;
+  line-height: 24px;
+}
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 46%;
+  object-fit: cover;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  outline: solid yellow 1px;
+
+}
+.swiper-button-next, .swiper-button-prev{
+    color: black;
+    border: none;
+    position:absolute;
+    background: none;
+}
+.swiper-button-next:after, .swiper-button-prev:after {
+  font-size: 10px;
+}
+
+</style>
--- src/vue-main.js
+++ src/vue-main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
-import './style/style.scss';
+import './style.scss';
 import App from './App.vue';
+import App2 from './AppSlideShow.vue';
 
 createApp(App).mount('#app');
+createApp(App2).mount('#slideApp');
